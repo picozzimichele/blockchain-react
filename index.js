b4d3c52c590526bd80a1f6916e613077607af60d
picozzimichele <michele.picozzi@hotmail.it>
2021-02-04 11:10:00 +0000

    Configure Production Code

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet");
 const TransactionMiner = require("./app/transaction-miner");
 
+const isDevelopment = process.env.ENV === 'development';
+
+const REDIS_URL = isDevelopment ? 
+    "redis://127.0.0.1:6379" : 
+    "redis://:p0d9dd98916111958fc1a3d93c592fa0f060c94a10d19eaad1fb80762394add07@ec2-3-210-15-236.compute-1.amazonaws.com:6989";
+    
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
+
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain, transactionPool });
+const pubsub = new PubSub({ blockchain, transactionPool, redisUrl: REDIS_URL });
 const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
-const DEFAULT_PORT = 3000;
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client/dist"))); //this is used to serve the js files into html format
@@ -105,45 +112,50 @@ const syncWithRootState = () => {
     })
 };
 
-//THIS IS ARBITRARY GENERATED DATA FOR TEST PURPOSES ONLY
 
-const walletFoo = new Wallet();
-const walletBar = new Wallet();
 
-const generateWalletTransaction = ({ wallet, recipient, amount }) => {
-    const transaction = wallet.createTransaction({ recipient, amount, chain: blockchain.chain});
+if (isDevelopment) {
 
-    transactionPool.setTransaction(transaction);
-};
+    //THIS IS ARBITRARY GENERATED DATA FOR TEST PURPOSES ONLY
 
-const walletAction = () => generateWalletTransaction({
-    wallet: wallet, recipient: walletFoo.publicKey, amount: 5
-})
+    const walletFoo = new Wallet();
+    const walletBar = new Wallet();
 
-const walletFooAction = () => generateWalletTransaction({
-    wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
-})
+    const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+        const transaction = wallet.createTransaction({ recipient, amount, chain: blockchain.chain});
 
-const walletBarAction = () => generateWalletTransaction({
-    wallet: walletBar, recipient: wallet.publicKey, amount: 15
-})
+        transactionPool.setTransaction(transaction);
+    };
 
-for (let i = 0; i < 10; i++) {
-    if (i % 3 === 0) {
-        walletAction();
-        walletFooAction();
-    } else if (i % 3 === 1) {
-        walletAction();
-        walletBarAction();
-    } else {
-        walletFooAction();
-        walletBarAction();
+    const walletAction = () => generateWalletTransaction({
+        wallet: wallet, recipient: walletFoo.publicKey, amount: 5
+    })
+
+    const walletFooAction = () => generateWalletTransaction({
+        wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+    })
+
+    const walletBarAction = () => generateWalletTransaction({
+        wallet: walletBar, recipient: wallet.publicKey, amount: 15
+    })
+
+    for (let i = 0; i < 10; i++) {
+        if (i % 3 === 0) {
+            walletAction();
+            walletFooAction();
+        } else if (i % 3 === 1) {
+            walletAction();
+            walletBarAction();
+        } else {
+            walletFooAction();
+            walletBarAction();
+        }
+
+        transactionMiner.mineTransaction();
     }
 
-    transactionMiner.mineTransaction();
+    //THIS IS ARBITRARY GENERATED DATA FOR TEST PURPOSES ONLY * END
 }
-
-//THIS IS ARBITRARY GENERATED DATA FOR TEST PURPOSES ONLY * END
 
 let PEER_PORT;
 
@@ -151,7 +163,7 @@ if(process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-const PORT = PEER_PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening on port localhost:${PORT}`);
 
